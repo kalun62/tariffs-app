7ac0103ffff32de2cb190d272184613fca50b267
Kalun <kalun62@fmail.com>
2026-04-09 11:57:07 +0300

    Prepare for GitHub Pages deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  output: 'export',
+  basePath: '/tariffs-app',
+  assetPrefix: '/my-tariffs-app/',
 };
 
 export default nextConfig;
